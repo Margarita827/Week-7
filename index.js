7cc540bd43c0fdab4a6429a3a7e8ef5d284b7e83
Margarita827 <margaiba5@gmail.com>
2023-12-05 09:08:20 +0000

    city name updated

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-alert("hello");
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-city");
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = searchInput.value;
+}
+
+let searchElement = document.querySelector("#search-form");
+searchElement.addEventListener("submit", handleSearchSubmit);
