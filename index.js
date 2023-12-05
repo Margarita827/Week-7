f0956514c239fb5d350688bab903070510ba4bd2
Margarita827 <margaiba5@gmail.com>
2023-12-05 09:53:42 +0000

    temperature changes

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#current-temperature");
+  let temperature = response.data.temperature.current;
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function changeCity(city) {
+  let apiKey = "o84c2a432ct7ef09733306be2507e42f";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-city");
-  let cityElement = document.querySelector("#current-city");
-  cityElement.innerHTML = searchInput.value;
+  changeCity(searchInput.value);
 }
 
 let searchElement = document.querySelector("#search-form");
 searchElement.addEventListener("submit", handleSearchSubmit);
+
+
+
